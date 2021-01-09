fc7b93e5053a3d5a4fe7d18bcff1cc583399ae7d
Mojatto <daichiandroid1997@gmail.com>
2021-01-09 11:36:17 +0900

    ライブラリ指定を追加．

--- ImguiDX11Test/Main.h
+++ ImguiDX11Test/Main.h
@@ -1,1 +1,18 @@
 #pragma once
+
+// Standard Library Includes
+#include <iostream>
+#include <string>
+
+// ImGui Library Includes
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx11.h"
+
+// Direct3D / DirectX Library Includes
+#include "D3D11.h"
+#include "D3DX11.h"
+
+namespace JattoImGui {
+	
+}
