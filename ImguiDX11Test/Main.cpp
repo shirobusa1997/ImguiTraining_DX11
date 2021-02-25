29076c338367179cec65f913ba0f13138694cb58
Mojatto_Dell <daichiandroid1997@gmail.com>
2021-02-25 20:52:33 +0900

    Imguiライブラリを追加．

--- ImguiDX11Test/Main.cpp
+++ ImguiDX11Test/Main.cpp
@@ -1,2 +1,27 @@
 #include "Main.h"
 
+bool JattoImGui::CreateDevice(HWND hWindow)
+{
+  return true;
+}
+
+void JattoImGui::CleanupDevice()
+{
+
+}
+
+void JattoImGui::CreateRenderTarget()
+{
+
+}
+
+void JattoImGui::CleanupRenderTarget()
+{
+
+}
+
+LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UNIT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI JattoImGui::WindowProc(HWND hWindow, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  
+}
--- ImguiDX11Test/Main.h
+++ ImguiDX11Test/Main.h
@@ -14,5 +14,24 @@
 #include "D3DX11.h"
 
 namespace JattoImGui {
-	
-}
+	// Direct3D Implement objects
+	static ID3D11Device*						pDevice						= NULL;
+	static ID3D11DeviceContext*			pDeviceContext		= NULL;
+	static IDXGISwapChain*					pSwapChain				= NULL;
+	static ID3D11RenderTargetView*	pRenderTargetView = NULL;
+
+	//
+	HWND hWindow;
+
+	// Flags
+	bool bShowGui = true;
+	bool bChekcbox;
+
+	// ClassMethods
+	static bool CreateDevice(HWND hWindowInst);
+	static void CleanupDevice();
+	static void CreateRenderTarget();
+	static void CleanupRenderTarget();
+
+	static LRESULT WINAPI WindowProc(HWND hWindow, UINT message, WPARAM wParam, LPARAM lParam);
+};
